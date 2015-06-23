d1ac2555a6c2f668c3b72655e884e7d19fd38538
Graeme Seaton <gseaton@gmail.com>
2015-06-23 15:45:35 +0100

    Change to ambari 2.0.1 to support HDP 2.2

--- modules/ambari_agent/manifests/init.pp
+++ modules/ambari_agent/manifests/init.pp
@@ -6,7 +6,7 @@ class ambari_agent ($ownhostname, $serverhostname) {
 
   # Ambari Repo
   exec { 'get-ambari-agent-repo':
-    command => "wget http://public-repo-1.hortonworks.com/ambari/centos6/1.x/updates/1.2.3.7/ambari.repo",
+    command => "wget http://public-repo-1.hortonworks.com/ambari/centos6/2.x/updates/2.0.1/ambari.repo",
     cwd     => '/etc/yum.repos.d/',
     creates => '/etc/yum.repos.d/ambari.repo',
     user    => root
